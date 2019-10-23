251c5ae1b7fc5b73af0105ab4b5745ec26d8d739
Kadyrova Ekaterina <puseva.ekaterina@gmail.com>
2019-10-22 23:55:35 -0400

    PrimitivesTest executed

---
+++ src/main/java/PrimitivesTest.java
@@ -0,0 +1,12 @@
+public class PrimitivesTest {
+    public static <Int> void main(String[] args) {
+        int b = 34;
+        int s = -34;
+        int i = 34;
+        long l = 65L;
+        float f = 12;
+        double myValue = 12.3;
+        boolean testMe = false;
+        char c = 'm';
+    }
+}
