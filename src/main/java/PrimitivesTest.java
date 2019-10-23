1d9d6dc1b6a2ce897af822a33c8243172ea4cfde
Kadyrova Ekaterina <puseva.ekaterina@gmail.com>
2019-10-23 18:45:09 -0400

    Changes committed in PrimitivesTest

--- src/main/java/PrimitivesTest.java
+++ src/main/java/PrimitivesTest.java
@@ -1,7 +1,7 @@
 public class PrimitivesTest {
     public static <Int> void main(String[] args) {
-        int b = 34;
-        int s = -34;
+        byte b = 34;
+        short s = -34;
         int i = 34;
         long l = 65L;
         float myValue = 12;
