781eb0b13a0f46f7d71c7b7d39d69e74ab92f6d0
Kadyrova Ekaterina <puseva.ekaterina@gmail.com>
2019-10-29 23:00:39 -0400

    8 possible combinations created

--- src/main/java/Rabbit.java
+++ src/main/java/Rabbit.java
@@ -1,19 +1,20 @@
+import javax.naming.Name;
 
 public class Rabbit {
 
-    String Name;
+    String name;
     int age;
     double weight;
 
     public String getName() {
-        return Name;
+        return name;
     }
 
     public void setName(String name) {
-        Name = name;
+        name = name;
     }
 
-        public int getAge() {
+    public int getAge() {
         return age;
     }
 
@@ -31,6 +32,42 @@ public class Rabbit {
     }
 
 
+    Rabbit() {
+    }
 
+    Rabbit(String name) {
+        this.name = name;
+    }
 
+    Rabbit(int age) {
+        this.age = age;
+    }
+
+    Rabbit(double weight) {
+        this.weight = weight;
+
+    }
+
+    Rabbit(String name, int age, double weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+
+    }
+
+    Rabbit(String name, double weight) {
+        this.name = name;
+        this.weight = weight;
+
+    }
+
+    Rabbit(int age, double weight) {
+        this.age = age;
+        this.weight = weight;
+    }
+
+    Rabbit(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
